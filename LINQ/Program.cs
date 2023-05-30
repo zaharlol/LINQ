60d75704a364da9d008434f7b2ee99f174cbbb70
zaharlol <kovtun.zahar12@mail.ru>
2023-05-30 14:28:24 +0300

    Добавьте файлы проекта.

---
+++ LINQ/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Linq;
+class Program 
+{
+    static void Main(string[] args)
+    {
+        Array array = new Array();
+        array.Array1();
+    }
+}
+class People 
+{
+public void People1()
+    {
+        string[] people = { "Анна", "Мария", "Сергей", "Алексей", "Дмитрий", "Ян" };
+
+
+        var selectedPeople = from p in people where p.StartsWith("А") orderby p select p;
+
+        foreach (string s in selectedPeople)
+            Console.WriteLine(s);
+    }
+}
+class Obj
+{
+    public void Obj1()
+    {
+        var objects = new List<object>()
+        {
+             1,
+             "Сергей ",
+             "Андрей ",
+             300,
+        };
+
+        foreach (var stringObject in objects.Where(o => o is string).OrderBy(o => o))
+            Console.WriteLine(stringObject);
+    }
+
+}
+public class City
+{
+    public City(string name, long population)
+    {
+        Name = name;
+        Population = population;
+    }
+
+    public string Name { get; set; }
+    public long Population { get; set; }
+
+    public void City1()
+    {
+        var russianCities = new List<City>();
+        russianCities.Add(new City("Москва", 11900000));
+        russianCities.Add(new City("Санкт-Петербург", 4991000));
+        russianCities.Add(new City("Волгоград", 1099000));
+        russianCities.Add(new City("Казань", 1169000));
+        russianCities.Add(new City("Севастополь", 449138));
+
+        var CityLenght = russianCities.Where(c => c.Name.Length > 10).OrderBy(c => c.Name);
+        foreach (var city in CityLenght)
+            Console.WriteLine(city);
+    }
+}
+class Numbers
+{
+    public void Numbers1()
+    {
+        string[] text = { "Раз два три",
+   "четыре пять шесть",
+   "семь восемь девять" };
+        var text2 = from num in text from word in num.Split(' ') select word;
+
+        foreach (var word in text2)
+            Console.WriteLine(word);
+    }
+}
+class Array
+{
+    public void Array1()
+    {
+        var numsList = new List<int[]>()
+{
+   new[] {2, 3, 7, 1},
+   new[] {45, 17, 88, 0},
+   new[] {23, 32, 44, -6},
+};
+        var num = from str in numsList from nu in str orderby nu select nu;
+
+        foreach (var q in num) 
+            Console.WriteLine(q);
+    }
+}
---
+++ LINQ`/Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Phone phone = new Phone();
+        phone.Phone1();
+    }
+}
+
+class Animal
+{
+    public void Animal1()
+    {
+        string[] words = { "Обезьяна", "Лягушка", "Кот", "Собака", "Черепаха" };
+        var ann = words.Select(x => new
+        {
+            Name = x,
+            Lenght = x.Length
+        })
+            .OrderBy(word => word.Lenght);
+
+        foreach (var word in ann) 
+        Console.WriteLine(word.Name + " " + word.Lenght);
+    }
+}
+public class Application
+{
+    public string Name { get; set; }
+    public int YearOfBirth { get; set; }
+}
+public class Student
+{
+    public string Name { get;  set; }
+    public int Age { get; set; }
+    public List<string> Languages { get; set; }
+}
+public class Coarse
+{
+    public string Name { get; set; }
+    public DateTime StartDate { get; set; }
+}
+public class Stud
+{
+    public void Stud1()
+    {
+        List<Student> students = new List<Student>
+{
+   new Student {Name="Андрей", Age=23, Languages = new List<string> {"английский", "немецкий" }},
+   new Student {Name="Сергей", Age=27, Languages = new List<string> {"английский", "французский" }},
+   new Student {Name="Дмитрий", Age=29, Languages = new List<string> {"английский", "испанский" }},
+   new Student {Name="Василий", Age=24, Languages = new List<string> {"испанский", "немецкий" }}
+};
+        var Mol = from s in students where s.Age < 27 
+                  let birdthay = DateTime.Now.Year - s.Age 
+                  select new Application
+                  {
+                      Name = s.Name,
+                      YearOfBirth = birdthay,
+                  };
+
+        foreach (var a in Mol) 
+            Console.WriteLine(a.Name + " " + a.YearOfBirth);
+    }
+}
+public class Studen
+{
+    public void Studen1()
+    {
+        var students = new List<Student>
+        {
+            new Student {Name="Андрей", Age=23, Languages = new List<string> {"английский", "немецкий" }},
+            new Student {Name="Сергей", Age=27, Languages = new List<string> {"английский", "французский" }},
+            new Student {Name="Дмитрий", Age=29, Languages = new List<string> {"английский", "испанский" }}
+        };
+        var coarses = new List<Coarse>
+        {
+             new Coarse {Name="Язык программирования C#", StartDate = new DateTime(2020, 12, 20)},
+             new Coarse {Name="Язык SQL и реляционные базы данных", StartDate = new DateTime(2020, 12, 15)},
+        };
+
+        var Mol = from s in students
+                  where s.Age < 29
+                  where s.Languages.Contains("английский")
+                  from a in coarses
+                  where a.Name.Contains("C#")
+                  let birdthay = DateTime.Now.Year - s.Age
+                  select new 
+                  {
+                      Name = s.Name,
+                      YearOfBirth = birdthay,
+                      NameCoarse = a.Name,
+                  };
+
+        foreach (var a in Mol)
+            Console.WriteLine(a.Name + " " + a.YearOfBirth + " " + a.NameCoarse);
+    }
+}
+public class Contact
+{
+    public string Name { get; set; }
+    public int Phone { get; set; }
+}
+class Cont
+{
+    public void Cont1()
+    {
+        var contacts = new List<Contact>()
+{
+   new Contact() { Name = "Андрей", Phone = 799994500 },
+   new Contact() { Name = "Сергей", Phone = 799990455 },
+   new Contact() { Name = "Иван", Phone = 79999675 },
+   new Contact() { Name = "Игорь", Phone = 8884994 },
+   new Contact() { Name = "Анна", Phone = 665565656 },
+   new Contact() { Name = "Василий", Phone = 3434 }
+};
+
+        while (true)
+        {
+            var keyChar = Console.ReadKey().KeyChar;
+            IEnumerable<Contact> page = null;
+            switch (keyChar)
+            {
+                case ('1'):
+                    page = contacts.Take(2);
+                    break;
+                case ('2'):
+                    page = contacts.Skip(2).Take(2);
+                    break;
+                case ('3'):
+                    page = contacts.Skip(4).Take(2);
+                    break;
+            }
+            foreach (var s in page)
+            {
+                Console.WriteLine(s.Name + " " + s.Phone);
+            }
+        }
+    }
+}
+public class Contact1 // модель класса
+{
+    public Contact1(string name, string lastName, long phoneNumber, String email) // метод-конструктор
+    {
+        Name = name;
+        LastName = lastName;
+        PhoneNumber = phoneNumber;
+        Email = email;
+    }
+
+    public String Name { get; }
+    public String LastName { get; }
+    public long PhoneNumber { get; }
+    public String Email { get; }
+}
+class Phone
+{
+    public void Phone1()
+    {
+        var phoneBook = new List<Contact1>();
+
+        // добавляем контакты
+        phoneBook.Add(new Contact1("Игорь", "Николаев", 79990000001, "igor@example.com"));
+        phoneBook.Add(new Contact1("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+        phoneBook.Add(new Contact1("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+        phoneBook.Add(new Contact1("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+        phoneBook.Add(new Contact1("Сергей", "Брин", 799900000013, "serg@example.com"));
+        phoneBook.Add(new Contact1("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+        while (true)
+        {
+            var keyChar = Console.ReadKey().KeyChar;
+            IEnumerable<Contact1> page = null;
+            switch (keyChar)
+            {
+                case ('1'):
+                    page = phoneBook.Take(2);
+                    break;
+                case ('2'):
+                    page = phoneBook.Skip(2).Take(2);
+                    break;
+                case ('3'):
+                    page = phoneBook.Skip(4).Take(2);
+                    break;
+            }
+            foreach (var s in page)
+            {
+                Console.WriteLine(s.Name + " " + s.LastName + " " + s.PhoneNumber + " " + s.Email);
+            }
+        }
+    }
+}
